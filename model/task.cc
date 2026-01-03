6893493b24193c9b6d15e615c7c87208e6add664
johnmullan <johnmullan1408@gmail.com>
2026-01-03 23:44:23 +0000

    distributed: Added a simple representation of a task

---
+++ model/task.cc
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2025 UCC
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: John Mullan <122331816@umail.ucc.ie>
+ */
+
+#include "task.h"
+
+#include "ns3/double.h"
+#include "ns3/log.h"
+#include "ns3/uinteger.h"
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("Task");
+NS_OBJECT_ENSURE_REGISTERED(Task);
+
+TypeId
+Task::GetTypeId()
+{
+    static TypeId tid =
+        TypeId("ns3::Task")
+            .SetParent<Object>()
+            .SetGroupName("Distributed")
+            .AddConstructor<Task>()
+            .AddAttribute("ComputeDemand",
+                          "Compute demand in FLOPS",
+                          DoubleValue(1e9),
+                          MakeDoubleAccessor(&Task::m_computeDemand),
+                          MakeDoubleChecker<double>(0))
+            .AddAttribute("InputSize",
+                          "Input data size in bytes",
+                          UintegerValue(1024),
+                          MakeUintegerAccessor(&Task::m_inputSize),
+                          MakeUintegerChecker<uint64_t>())
+            .AddAttribute("OutputSize",
+                          "Output data size in bytes",
+                          UintegerValue(1024),
+                          MakeUintegerAccessor(&Task::m_outputSize),
+                          MakeUintegerChecker<uint64_t>());
+    return tid;
+}
+
+Task::Task()
+    : m_computeDemand(1e9),
+      m_inputSize(1024),
+      m_outputSize(1024),
+      m_arrivalTime(Seconds(0)),
+      m_taskId(0)
+{
+    NS_LOG_FUNCTION(this);
+}
+
+Task::~Task()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+void
+Task::SetComputeDemand(double flops)
+{
+    NS_LOG_FUNCTION(this << flops);
+    m_computeDemand = flops;
+}
+
+void
+Task::SetInputSize(uint64_t bytes)
+{
+    NS_LOG_FUNCTION(this << bytes);
+    m_inputSize = bytes;
+}
+
+void
+Task::SetOutputSize(uint64_t bytes)
+{
+    NS_LOG_FUNCTION(this << bytes);
+    m_outputSize = bytes;
+}
+
+void
+Task::SetArrivalTime(Time time)
+{
+    NS_LOG_FUNCTION(this << time);
+    m_arrivalTime = time;
+}
+
+void
+Task::SetTaskId(uint64_t id)
+{
+    NS_LOG_FUNCTION(this << id);
+    m_taskId = id;
+}
+
+double
+Task::GetComputeDemand() const
+{
+    return m_computeDemand;
+}
+
+uint64_t
+Task::GetInputSize() const
+{
+    return m_inputSize;
+}
+
+uint64_t
+Task::GetOutputSize() const
+{
+    return m_outputSize;
+}
+
+Time
+Task::GetArrivalTime() const
+{
+    return m_arrivalTime;
+}
+
+uint64_t
+Task::GetTaskId() const
+{
+    return m_taskId;
+}
+
+} // namespace ns3
---
+++ model/task.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2025 UCC
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: John Mullan <122331816@umail.ucc.ie>
+ */
+
+#ifndef TASK_H
+#define TASK_H
+
+#include "ns3/nstime.h"
+#include "ns3/object.h"
+#include "ns3/ptr.h"
+
+namespace ns3
+{
+
+/**
+ * @ingroup distributed
+ * @brief Represents a computational task to be executed on an accelerator.
+ *
+ * A Task encapsulates the compute demand (in FLOPS), input/output data sizes,
+ * and timing metadata for simulation of distributed computing workloads.
+ */
+class Task : public Object
+{
+  public:
+    /**
+     * @brief Get the type ID.
+     * @return The object TypeId.
+     */
+    static TypeId GetTypeId();
+
+    Task();
+    ~Task() override;
+
+    /**
+     * @brief Set the compute demand in FLOPS.
+     * @param flops The compute demand.
+     */
+    void SetComputeDemand(double flops);
+
+    /**
+     * @brief Set the input data size in bytes.
+     * @param bytes The input size.
+     */
+    void SetInputSize(uint64_t bytes);
+
+    /**
+     * @brief Set the output data size in bytes.
+     * @param bytes The output size.
+     */
+    void SetOutputSize(uint64_t bytes);
+
+    /**
+     * @brief Set the task arrival time.
+     * @param time The arrival time.
+     */
+    void SetArrivalTime(Time time);
+
+    /**
+     * @brief Set the unique task identifier.
+     * @param id The task ID.
+     */
+    void SetTaskId(uint64_t id);
+
+    /**
+     * @brief Get the compute demand in FLOPS.
+     * @return The compute demand.
+     */
+    double GetComputeDemand() const;
+
+    /**
+     * @brief Get the input data size in bytes.
+     * @return The input size.
+     */
+    uint64_t GetInputSize() const;
+
+    /**
+     * @brief Get the output data size in bytes.
+     * @return The output size.
+     */
+    uint64_t GetOutputSize() const;
+
+    /**
+     * @brief Get the task arrival time.
+     * @return The arrival time.
+     */
+    Time GetArrivalTime() const;
+
+    /**
+     * @brief Get the unique task identifier.
+     * @return The task ID.
+     */
+    uint64_t GetTaskId() const;
+
+  private:
+    double m_computeDemand;   //!< Compute demand in FLOPS
+    uint64_t m_inputSize;     //!< Input data size in bytes
+    uint64_t m_outputSize;    //!< Output data size in bytes
+    Time m_arrivalTime;       //!< Time when task arrived
+    uint64_t m_taskId;        //!< Unique task identifier
+};
+
+} // namespace ns3
+
+#endif // TASK_H
