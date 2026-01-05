105b04d70cf7d687a9d71f9225ae5c850f1adcda
johnmullan <johnmullan1408@gmail.com>
2026-01-05 03:37:08 +0000

    distributed: Added a OffloadServer based on the application implementation in packet-sink.cc/h
    - The offload server creates a task object from the received header and submits the task to the accelerator.
    - The offload server sends TASK_RESPONSE messages on completion of tasks.

---
+++ model/offload-server.cc
@@ -0,0 +1,479 @@
+/*
+ * Copyright (c) 2025 UCC
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: John Mullan <122331816@umail.ucc.ie>
+ */
+
+#include "offload-server.h"
+
+#include "ns3/address-utils.h"
+#include "ns3/log.h"
+#include "ns3/node.h"
+#include "ns3/packet.h"
+#include "ns3/simulator.h"
+#include "ns3/socket-factory.h"
+#include "ns3/socket.h"
+#include "ns3/tcp-socket-factory.h"
+#include "ns3/uinteger.h"
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("OffloadServer");
+
+NS_OBJECT_ENSURE_REGISTERED(OffloadServer);
+
+TypeId
+OffloadServer::GetTypeId()
+{
+    static TypeId tid =
+        TypeId("ns3::OffloadServer")
+            .SetParent<Application>()
+            .SetGroupName("Distributed")
+            .AddConstructor<OffloadServer>()
+            .AddAttribute("Local",
+                          "The local address to bind to",
+                          AddressValue(),
+                          MakeAddressAccessor(&OffloadServer::m_local),
+                          MakeAddressChecker())
+            .AddAttribute("Port",
+                          "Port on which to listen for incoming connections",
+                          UintegerValue(9000),
+                          MakeUintegerAccessor(&OffloadServer::m_port),
+                          MakeUintegerChecker<uint16_t>())
+            .AddTraceSource("TaskReceived",
+                            "A task request has been received",
+                            MakeTraceSourceAccessor(&OffloadServer::m_taskReceivedTrace),
+                            "ns3::OffloadServer::TaskReceivedTracedCallback")
+            .AddTraceSource("TaskCompleted",
+                            "A task has been completed and response sent",
+                            MakeTraceSourceAccessor(&OffloadServer::m_taskCompletedTrace),
+                            "ns3::OffloadServer::TaskCompletedTracedCallback");
+    return tid;
+}
+
+OffloadServer::OffloadServer()
+    : m_port(9000),
+      m_socket(nullptr),
+      m_socket6(nullptr),
+      m_accelerator(nullptr),
+      m_tasksReceived(0),
+      m_tasksCompleted(0),
+      m_totalRx(0)
+{
+    NS_LOG_FUNCTION(this);
+}
+
+OffloadServer::~OffloadServer()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+void
+OffloadServer::DoDispose()
+{
+    NS_LOG_FUNCTION(this);
+    m_socket = nullptr;
+    m_socket6 = nullptr;
+    m_socketList.clear();
+    m_rxBuffer.clear();
+    m_pendingTasks.clear();
+    m_accelerator = nullptr;
+    Application::DoDispose();
+}
+
+uint64_t
+OffloadServer::GetTasksReceived() const
+{
+    return m_tasksReceived;
+}
+
+uint64_t
+OffloadServer::GetTasksCompleted() const
+{
+    return m_tasksCompleted;
+}
+
+uint64_t
+OffloadServer::GetTotalRx() const
+{
+    return m_totalRx;
+}
+
+void
+OffloadServer::StartApplication()
+{
+    NS_LOG_FUNCTION(this);
+
+    // Get the GpuAccelerator aggregated to this node
+    m_accelerator = GetNode()->GetObject<GpuAccelerator>();
+    if (!m_accelerator)
+    {
+        NS_LOG_WARN("No GpuAccelerator aggregated to this node. Tasks will be rejected.");
+    }
+    else
+    {
+        // Subscribe to task completion events
+        m_accelerator->TraceConnectWithoutContext(
+            "TaskCompleted",
+            MakeCallback(&OffloadServer::OnTaskCompleted, this));
+    }
+
+    // Create the listening socket
+    if (!m_socket)
+    {
+        m_socket = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
+
+        Address local = m_local;
+        if (local.IsInvalid())
+        {
+            local = InetSocketAddress(Ipv4Address::GetAny(), m_port);
+            NS_LOG_INFO("Binding to port " << m_port);
+        }
+        else if (InetSocketAddress::IsMatchingType(local))
+        {
+            m_port = InetSocketAddress::ConvertFrom(local).GetPort();
+            NS_LOG_INFO("Binding to " << InetSocketAddress::ConvertFrom(local).GetIpv4()
+                                      << ":" << m_port);
+        }
+        else if (Inet6SocketAddress::IsMatchingType(local))
+        {
+            m_port = Inet6SocketAddress::ConvertFrom(local).GetPort();
+            NS_LOG_INFO("Binding to " << Inet6SocketAddress::ConvertFrom(local).GetIpv6()
+                                      << ":" << m_port);
+        }
+
+        if (m_socket->Bind(local) == -1)
+        {
+            NS_FATAL_ERROR("Failed to bind socket");
+        }
+
+        m_socket->Listen();
+        // NOTE: Unlike PacketSink, we do NOT call ShutdownSend() here because
+        // OffloadServer needs to send TASK_RESPONSE messages back to clients
+        // when tasks complete on the GPU.
+        m_socket->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
+                                    MakeCallback(&OffloadServer::HandleAccept, this));
+        m_socket->SetCloseCallbacks(MakeCallback(&OffloadServer::HandlePeerClose, this),
+                                    MakeCallback(&OffloadServer::HandlePeerError, this));
+    }
+
+    // Create IPv6 socket if only port was specified (for dual-stack support)
+    if (!m_socket6 && m_local.IsInvalid())
+    {
+        m_socket6 = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
+        Address local6 = Inet6SocketAddress(Ipv6Address::GetAny(), m_port);
+
+        if (m_socket6->Bind(local6) == -1)
+        {
+            NS_LOG_WARN("Failed to bind IPv6 socket");
+            m_socket6 = nullptr;
+        }
+        else
+        {
+            m_socket6->Listen();
+            // NOTE: No ShutdownSend() - we need to send responses on IPv6 too
+            m_socket6->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
+                                         MakeCallback(&OffloadServer::HandleAccept, this));
+            m_socket6->SetCloseCallbacks(MakeCallback(&OffloadServer::HandlePeerClose, this),
+                                         MakeCallback(&OffloadServer::HandlePeerError, this));
+        }
+    }
+}
+
+void
+OffloadServer::StopApplication()
+{
+    NS_LOG_FUNCTION(this);
+
+    // Disconnect from accelerator
+    if (m_accelerator)
+    {
+        m_accelerator->TraceDisconnectWithoutContext(
+            "TaskCompleted",
+            MakeCallback(&OffloadServer::OnTaskCompleted, this));
+    }
+
+    // Close listening sockets
+    if (m_socket)
+    {
+        m_socket->Close();
+        m_socket->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
+                                    MakeNullCallback<void, Ptr<Socket>, const Address&>());
+    }
+    if (m_socket6)
+    {
+        m_socket6->Close();
+        m_socket6->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
+                                     MakeNullCallback<void, Ptr<Socket>, const Address&>());
+    }
+
+    // Close all accepted sockets
+    for (auto& socket : m_socketList)
+    {
+        socket->Close();
+    }
+    m_socketList.clear();
+}
+
+void
+OffloadServer::HandleAccept(Ptr<Socket> socket, const Address& from)
+{
+    NS_LOG_FUNCTION(this << socket << from);
+    NS_LOG_INFO("Accepted connection from " << from);
+
+    socket->SetRecvCallback(MakeCallback(&OffloadServer::HandleRead, this));
+    socket->SetCloseCallbacks(MakeCallback(&OffloadServer::HandlePeerClose, this),
+                              MakeCallback(&OffloadServer::HandlePeerError, this));
+    m_socketList.push_back(socket);
+}
+
+void
+OffloadServer::HandleRead(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+
+    Ptr<Packet> packet;
+    Address from;
+
+    while ((packet = socket->RecvFrom(from)))
+    {
+        if (packet->GetSize() == 0)
+        {
+            break;
+        }
+
+        m_totalRx += packet->GetSize();
+        NS_LOG_DEBUG("Received " << packet->GetSize() << " bytes from " << from);
+
+        // Add to buffer for this client (TCP stream reassembly)
+        auto it = m_rxBuffer.find(from);
+        if (it == m_rxBuffer.end())
+        {
+            m_rxBuffer[from] = packet;
+        }
+        else
+        {
+            it->second->AddAtEnd(packet);
+        }
+
+        // Try to process complete messages from the buffer
+        ProcessBuffer(socket, from);
+    }
+}
+
+void
+OffloadServer::ProcessBuffer(Ptr<Socket> socket, const Address& from)
+{
+    NS_LOG_FUNCTION(this << socket << from);
+
+    auto it = m_rxBuffer.find(from);
+    if (it == m_rxBuffer.end())
+    {
+        return;
+    }
+
+    Ptr<Packet> buffer = it->second;
+
+    // Process all complete messages in the buffer
+    while (buffer->GetSize() > 0)
+    {
+        // Check if we have enough for the header
+        OffloadHeader header;
+        uint32_t headerSize = header.GetSerializedSize();
+
+        if (buffer->GetSize() < headerSize)
+        {
+            NS_LOG_DEBUG("Buffer has " << buffer->GetSize()
+                                       << " bytes, need " << headerSize << " for header");
+            break;
+        }
+
+        // Peek at the header to get input size
+        buffer->PeekHeader(header);
+
+        // Calculate total message size (header + payload)
+        uint64_t inputSize = header.GetInputSize();
+        uint64_t totalMessageSize = headerSize + inputSize;
+
+        // Ensure we have the complete message
+        if (totalMessageSize > buffer->GetSize())
+        {
+            NS_LOG_DEBUG("Buffer has " << buffer->GetSize()
+                                       << " bytes, need " << totalMessageSize << " for full message");
+            break;
+        }
+
+        // Remove the header from the buffer
+        buffer->RemoveHeader(header);
+
+        // Remove the payload (we don't need the actual data, just consume it)
+        if (inputSize > 0 && inputSize <= buffer->GetSize())
+        {
+            buffer->RemoveAtStart(inputSize);
+        }
+
+        // Process the task
+        ProcessTask(header, socket);
+    }
+
+    // Update or remove the buffer entry
+    if (buffer->GetSize() == 0)
+    {
+        m_rxBuffer.erase(it);
+    }
+}
+
+void
+OffloadServer::ProcessTask(const OffloadHeader& header, Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << header.GetTaskId() << socket);
+
+    if (header.GetMessageType() != OffloadHeader::TASK_REQUEST)
+    {
+        NS_LOG_WARN("Received non-request message type, ignoring");
+        return;
+    }
+
+    m_tasksReceived++;
+    m_taskReceivedTrace(header);
+
+    NS_LOG_INFO("Received task " << header.GetTaskId()
+                                 << " (compute=" << header.GetComputeDemand()
+                                 << ", input=" << header.GetInputSize()
+                                 << ", output=" << header.GetOutputSize() << ")");
+
+    if (!m_accelerator)
+    {
+        NS_LOG_WARN("No accelerator available, dropping task " << header.GetTaskId());
+        return;
+    }
+
+    // Create a Task object from the header
+    Ptr<Task> task = CreateObject<Task>();
+    task->SetTaskId(header.GetTaskId());
+    task->SetComputeDemand(header.GetComputeDemand());
+    task->SetInputSize(header.GetInputSize());
+    task->SetOutputSize(header.GetOutputSize());
+    task->SetArrivalTime(Simulator::Now());
+
+    // Track the pending task for response routing
+    PendingTask pending;
+    pending.socket = socket;
+    pending.task = task;
+    m_pendingTasks[header.GetTaskId()] = pending;
+
+    // Submit to the accelerator
+    m_accelerator->SubmitTask(task);
+
+    NS_LOG_DEBUG("Submitted task " << header.GetTaskId() << " to accelerator");
+}
+
+void
+OffloadServer::OnTaskCompleted(Ptr<const Task> task, Time duration)
+{
+    NS_LOG_FUNCTION(this << task->GetTaskId() << duration);
+
+    auto it = m_pendingTasks.find(task->GetTaskId());
+    if (it == m_pendingTasks.end())
+    {
+        // Task not found in our pending map - it may have been submitted by
+        // another source (e.g., TaskGenerator) directly to the accelerator
+        NS_LOG_DEBUG("Task " << task->GetTaskId() << " not found in pending tasks (not ours)");
+        return;
+    }
+
+    Ptr<Socket> socket = it->second.socket;
+    m_pendingTasks.erase(it);
+
+    // Check if socket is still valid
+    if (socket)
+    {
+        SendResponse(socket, task, duration);
+    }
+    else
+    {
+        NS_LOG_WARN("Client socket closed before task " << task->GetTaskId() << " completed");
+    }
+}
+
+void
+OffloadServer::SendResponse(Ptr<Socket> socket, Ptr<const Task> task, Time duration)
+{
+    NS_LOG_FUNCTION(this << socket << task->GetTaskId() << duration);
+
+    // Create response header
+    OffloadHeader response;
+    response.SetMessageType(OffloadHeader::TASK_RESPONSE);
+    response.SetTaskId(task->GetTaskId());
+    response.SetComputeDemand(task->GetComputeDemand());
+    response.SetInputSize(task->GetInputSize());
+    response.SetOutputSize(task->GetOutputSize());
+
+    // Create packet with output payload to simulate result data transfer
+    uint64_t outputSize = task->GetOutputSize();
+    Ptr<Packet> packet = Create<Packet>(outputSize);
+    packet->AddHeader(response);
+
+    // Send response
+    int sent = socket->Send(packet);
+    if (sent > 0)
+    {
+        m_tasksCompleted++;
+        m_taskCompletedTrace(response, duration);
+
+        NS_LOG_INFO("Sent response for task " << task->GetTaskId()
+                                              << " (output=" << outputSize << " bytes"
+                                              << ", duration=" << duration.GetMilliSeconds() << "ms)");
+    }
+    else
+    {
+        NS_LOG_ERROR("Failed to send response for task " << task->GetTaskId());
+    }
+}
+
+void
+OffloadServer::HandlePeerClose(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+    NS_LOG_INFO("Client disconnected");
+    CleanupSocket(socket);
+}
+
+void
+OffloadServer::HandlePeerError(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+    NS_LOG_WARN("Socket error");
+    CleanupSocket(socket);
+}
+
+void
+OffloadServer::CleanupSocket(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+
+    // Remove from socket list
+    m_socketList.remove(socket);
+
+    // Clear pending tasks for this socket
+    for (auto it = m_pendingTasks.begin(); it != m_pendingTasks.end();)
+    {
+        if (it->second.socket == socket)
+        {
+            NS_LOG_DEBUG("Removing pending task " << it->first << " for closed socket");
+            it = m_pendingTasks.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+
+    // Note: We don't clear the rx buffer here as it's keyed by Address, not Socket.
+    // Stale buffer entries will be overwritten if the same client reconnects.
+}
+
+} // namespace ns3
---
+++ model/offload-server.h
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2025 UCC
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: John Mullan <122331816@umail.ucc.ie>
+ */
+
+#ifndef OFFLOAD_SERVER_H
+#define OFFLOAD_SERVER_H
+
+#include "gpu-accelerator.h"
+#include "offload-header.h"
+#include "task.h"
+
+#include "ns3/address.h"
+#include "ns3/application.h"
+#include "ns3/inet-socket-address.h"
+#include "ns3/inet6-socket-address.h"
+#include "ns3/ptr.h"
+#include "ns3/traced-callback.h"
+
+#include <list>
+#include <unordered_map>
+
+namespace ns3
+{
+
+class Socket;
+class Packet;
+
+/**
+ * @ingroup distributed
+ * @brief TCP server application for receiving and processing offloaded tasks.
+ *
+ * OffloadServer listens for TCP connections from OffloadClient applications,
+ * receives task requests, submits them to the GpuAccelerator aggregated to
+ * the node, and sends responses back when tasks complete.
+ *
+ * Since TCP is a stream protocol, the server buffers incoming data per-client
+ * and extracts complete OffloadHeader messages as they arrive.
+ */
+class OffloadServer : public Application
+{
+  public:
+    /**
+     * @brief Get the type ID.
+     * @return The object TypeId.
+     */
+    static TypeId GetTypeId();
+
+    OffloadServer();
+    ~OffloadServer() override;
+
+    /**
+     * @brief Get the number of tasks received.
+     * @return Number of tasks received.
+     */
+    uint64_t GetTasksReceived() const;
+
+    /**
+     * @brief Get the number of tasks completed.
+     * @return Number of tasks completed.
+     */
+    uint64_t GetTasksCompleted() const;
+
+    /**
+     * @brief Get the total bytes received.
+     * @return Total bytes received.
+     */
+    uint64_t GetTotalRx() const;
+
+    /**
+     * @brief TracedCallback signature for task received events.
+     * @param header The offload header that was received.
+     */
+    typedef void (*TaskReceivedTracedCallback)(const OffloadHeader& header);
+
+    /**
+     * @brief TracedCallback signature for task completed events.
+     * @param header The offload header for the response.
+     * @param duration The processing duration.
+     */
+    typedef void (*TaskCompletedTracedCallback)(const OffloadHeader& header, Time duration);
+
+  protected:
+    void DoDispose() override;
+
+  private:
+    void StartApplication() override;
+    void StopApplication() override;
+
+    /**
+     * @brief Handle a packet received on a socket.
+     * @param socket The receiving socket.
+     */
+    void HandleRead(Ptr<Socket> socket);
+
+    /**
+     * @brief Handle an incoming connection.
+     * @param socket The new connection socket.
+     * @param from The address of the client.
+     */
+    void HandleAccept(Ptr<Socket> socket, const Address& from);
+
+    /**
+     * @brief Handle a connection close.
+     * @param socket The closed socket.
+     */
+    void HandlePeerClose(Ptr<Socket> socket);
+
+    /**
+     * @brief Handle a connection error.
+     * @param socket The socket with error.
+     */
+    void HandlePeerError(Ptr<Socket> socket);
+
+    /**
+     * @brief Process buffered data for a client, extracting complete messages.
+     * @param socket The client socket.
+     * @param from The client address.
+     */
+    void ProcessBuffer(Ptr<Socket> socket, const Address& from);
+
+    /**
+     * @brief Process a complete task request.
+     * @param header The offload header.
+     * @param socket The client socket.
+     */
+    void ProcessTask(const OffloadHeader& header, Ptr<Socket> socket);
+
+    /**
+     * @brief Called when a task completes on the GPU.
+     * @param task The completed task.
+     * @param duration The processing duration.
+     */
+    void OnTaskCompleted(Ptr<const Task> task, Time duration);
+
+    /**
+     * @brief Send a response to the client.
+     * @param socket The client socket.
+     * @param task The completed task.
+     * @param duration The processing duration.
+     */
+    void SendResponse(Ptr<Socket> socket, Ptr<const Task> task, Time duration);
+
+    /**
+     * @brief Clean up pending tasks for a closed socket.
+     * @param socket The closed socket.
+     */
+    void CleanupSocket(Ptr<Socket> socket);
+
+    /**
+     * @brief Hashing for the Address class (for buffer map).
+     */
+    struct AddressHash
+    {
+        /**
+         * @brief Hash operator for Address.
+         * @param x The address to hash.
+         * @return The hash value.
+         */
+        size_t operator()(const Address& x) const
+        {
+            if (InetSocketAddress::IsMatchingType(x))
+            {
+                InetSocketAddress a = InetSocketAddress::ConvertFrom(x);
+                return Ipv4AddressHash()(a.GetIpv4());
+            }
+            else if (Inet6SocketAddress::IsMatchingType(x))
+            {
+                Inet6SocketAddress a = Inet6SocketAddress::ConvertFrom(x);
+                return Ipv6AddressHash()(a.GetIpv6());
+            }
+            return 0;
+        }
+    };
+
+    // Configuration
+    Address m_local;   //!< Local address to bind to
+    uint16_t m_port;   //!< Port to listen on
+
+    // Sockets
+    Ptr<Socket> m_socket;   //!< IPv4 listening socket
+    Ptr<Socket> m_socket6;  //!< IPv6 listening socket (used if only port is specified)
+    std::list<Ptr<Socket>> m_socketList; //!< Accepted client sockets
+
+    // Per-client receive buffers (for TCP stream reassembly)
+    std::unordered_map<Address, Ptr<Packet>, AddressHash> m_rxBuffer;
+
+    // GPU accelerator
+    Ptr<GpuAccelerator> m_accelerator;  //!< Cached accelerator reference
+
+    // Pending tasks: taskId -> (socket, task)
+    struct PendingTask
+    {
+        Ptr<Socket> socket;
+        Ptr<Task> task;
+    };
+    std::unordered_map<uint64_t, PendingTask> m_pendingTasks;
+
+    // Statistics
+    uint64_t m_tasksReceived;   //!< Number of tasks received
+    uint64_t m_tasksCompleted;  //!< Number of tasks completed
+    uint64_t m_totalRx;         //!< Total bytes received
+
+    // Trace sources
+    TracedCallback<const OffloadHeader&> m_taskReceivedTrace;        //!< Task received
+    TracedCallback<const OffloadHeader&, Time> m_taskCompletedTrace; //!< Task completed
+};
+
+} // namespace ns3
+
+#endif // OFFLOAD_SERVER_H
