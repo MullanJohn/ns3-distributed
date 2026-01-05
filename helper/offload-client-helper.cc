f4211c9e4555b6e84c785f2f594b24c8245d1f62
johnmullan <johnmullan1408@gmail.com>
2026-01-05 02:02:31 +0000

    distributed: Added a helper class for OffloadClient applications

---
+++ helper/offload-client-helper.cc
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2025 UCC
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: John Mullan <122331816@umail.ucc.ie>
+ */
+
+#include "offload-client-helper.h"
+
+#include "ns3/address.h"
+#include "ns3/string.h"
+#include "ns3/uinteger.h"
+
+#include <sstream>
+
+namespace ns3
+{
+
+OffloadClientHelper::OffloadClientHelper()
+    : ApplicationHelper("ns3::OffloadClient")
+{
+}
+
+OffloadClientHelper::OffloadClientHelper(const Address& serverAddress)
+    : ApplicationHelper("ns3::OffloadClient")
+{
+    m_factory.Set("Remote", AddressValue(serverAddress));
+}
+
+void
+OffloadClientHelper::SetServerAddress(const Address& serverAddress)
+{
+    m_factory.Set("Remote", AddressValue(serverAddress));
+}
+
+void
+OffloadClientHelper::SetMeanInterval(double mean)
+{
+    std::ostringstream oss;
+    oss << "ns3::ExponentialRandomVariable[Mean=" << mean << "]";
+    m_factory.Set("InterArrivalTime", StringValue(oss.str()));
+}
+
+void
+OffloadClientHelper::SetMeanComputeDemand(double mean)
+{
+    std::ostringstream oss;
+    oss << "ns3::ExponentialRandomVariable[Mean=" << mean << "]";
+    m_factory.Set("ComputeDemand", StringValue(oss.str()));
+}
+
+void
+OffloadClientHelper::SetMeanInputSize(double mean)
+{
+    std::ostringstream oss;
+    oss << "ns3::ExponentialRandomVariable[Mean=" << mean << "]";
+    m_factory.Set("InputSize", StringValue(oss.str()));
+}
+
+void
+OffloadClientHelper::SetMeanOutputSize(double mean)
+{
+    std::ostringstream oss;
+    oss << "ns3::ExponentialRandomVariable[Mean=" << mean << "]";
+    m_factory.Set("OutputSize", StringValue(oss.str()));
+}
+
+void
+OffloadClientHelper::SetMaxTasks(uint64_t maxTasks)
+{
+    m_factory.Set("MaxTasks", UintegerValue(maxTasks));
+}
+
+} // namespace ns3
---
+++ helper/offload-client-helper.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2025 UCC
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: John Mullan <122331816@umail.ucc.ie>
+ */
+
+#ifndef OFFLOAD_CLIENT_HELPER_H
+#define OFFLOAD_CLIENT_HELPER_H
+
+#include "ns3/address.h"
+#include "ns3/application-helper.h"
+
+namespace ns3
+{
+
+/**
+ * @ingroup distributed
+ * @brief Helper to create and configure OffloadClient applications.
+ *
+ * This helper simplifies the creation and configuration of OffloadClient
+ * applications for distributed computing simulations.
+ */
+class OffloadClientHelper : public ApplicationHelper
+{
+  public:
+    /**
+     * @brief Construct helper with default OffloadClient type.
+     */
+    OffloadClientHelper();
+
+    /**
+     * @brief Construct helper with target server address.
+     * @param serverAddress Address of the offload server.
+     */
+    explicit OffloadClientHelper(const Address& serverAddress);
+
+    /**
+     * @brief Set the remote server address.
+     * @param serverAddress Address of the offload server.
+     */
+    void SetServerAddress(const Address& serverAddress);
+
+    /**
+     * @brief Set the mean inter-arrival time between tasks.
+     * @param mean Mean inter-arrival time in seconds.
+     */
+    void SetMeanInterval(double mean);
+
+    /**
+     * @brief Set the mean compute demand.
+     * @param mean Mean compute demand in FLOPS.
+     */
+    void SetMeanComputeDemand(double mean);
+
+    /**
+     * @brief Set the mean input size.
+     * @param mean Mean input size in bytes.
+     */
+    void SetMeanInputSize(double mean);
+
+    /**
+     * @brief Set the mean output size.
+     * @param mean Mean output size in bytes.
+     */
+    void SetMeanOutputSize(double mean);
+
+    /**
+     * @brief Set maximum number of tasks to send.
+     * @param maxTasks Maximum task count (0 = unlimited).
+     */
+    void SetMaxTasks(uint64_t maxTasks);
+};
+
+} // namespace ns3
+
+#endif // OFFLOAD_CLIENT_HELPER_H
