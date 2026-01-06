8f4b27451f899247f07e4bbe1135712b217e87f4
johnmullan <johnmullan1408@gmail.com>
2026-01-06 02:10:18 +0000

    distributed: Added a load balancer application
    - Load balancer maintains a listening socket for incoming clients, persistent tcp connections to backend servers  and a map to perform response routing.
    - The load balancer's methods are split between client facing and backend facing methods.
    - The load balancer provides some collected statistics and trace sources for users.
    - The load balancer takes a scheduler as a member to be used in making scheduling decisions.

---
+++ model/load-balancer.cc
@@ -0,0 +1,624 @@
+/*
+ * Copyright (c) 2025 UCC
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: John Mullan <122331816@umail.ucc.ie>
+ */
+
+#include "load-balancer.h"
+
+#include "round-robin-scheduler.h"
+
+#include "ns3/log.h"
+#include "ns3/pointer.h"
+#include "ns3/simulator.h"
+#include "ns3/socket-factory.h"
+#include "ns3/tcp-socket-factory.h"
+#include "ns3/uinteger.h"
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("LoadBalancer");
+
+NS_OBJECT_ENSURE_REGISTERED(LoadBalancer);
+
+// AddressHash implementation
+std::size_t
+LoadBalancer::AddressHash::operator()(const Address& addr) const
+{
+    // Simple hash based on address bytes
+    uint8_t buffer[Address::MAX_SIZE];
+    uint32_t len = addr.CopyTo(buffer);
+    std::size_t hash = 0;
+    for (uint32_t i = 0; i < len; i++)
+    {
+        hash = hash * 31 + buffer[i];
+    }
+    return hash;
+}
+
+TypeId
+LoadBalancer::GetTypeId()
+{
+    static TypeId tid =
+        TypeId("ns3::LoadBalancer")
+            .SetParent<Application>()
+            .SetGroupName("Distributed")
+            .AddConstructor<LoadBalancer>()
+            .AddAttribute("Port",
+                          "Port to listen on for client connections",
+                          UintegerValue(8000),
+                          MakeUintegerAccessor(&LoadBalancer::m_port),
+                          MakeUintegerChecker<uint16_t>())
+            .AddAttribute("Scheduler",
+                          "The node scheduler to use for backend selection",
+                          PointerValue(CreateObject<RoundRobinScheduler>()),
+                          MakePointerAccessor(&LoadBalancer::m_scheduler),
+                          MakePointerChecker<NodeScheduler>())
+            .AddTraceSource("TaskForwarded",
+                            "A task was forwarded to a backend",
+                            MakeTraceSourceAccessor(&LoadBalancer::m_taskForwardedTrace),
+                            "ns3::LoadBalancer::TaskForwardedCallback")
+            .AddTraceSource("ResponseRouted",
+                            "A response was routed to a client",
+                            MakeTraceSourceAccessor(&LoadBalancer::m_responseRoutedTrace),
+                            "ns3::LoadBalancer::ResponseRoutedCallback");
+    return tid;
+}
+
+LoadBalancer::LoadBalancer()
+    : m_port(8000),
+      m_scheduler(nullptr),
+      m_listenSocket(nullptr),
+      m_tasksForwarded(0),
+      m_responsesRouted(0),
+      m_clientRx(0),
+      m_backendRx(0)
+{
+    NS_LOG_FUNCTION(this);
+}
+
+LoadBalancer::~LoadBalancer()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+void
+LoadBalancer::DoDispose()
+{
+    NS_LOG_FUNCTION(this);
+    m_listenSocket = nullptr;
+    m_clientSockets.clear();
+    m_clientRxBuffers.clear();
+    m_backendSockets.clear();
+    m_backendConnected.clear();
+    m_backendRxBuffers.clear();
+    m_pendingResponses.clear();
+    m_scheduler = nullptr;
+    Application::DoDispose();
+}
+
+void
+LoadBalancer::SetCluster(const Cluster& cluster)
+{
+    NS_LOG_FUNCTION(this << cluster.GetN());
+    m_cluster = cluster;
+}
+
+uint64_t
+LoadBalancer::GetTasksForwarded() const
+{
+    return m_tasksForwarded;
+}
+
+uint64_t
+LoadBalancer::GetResponsesRouted() const
+{
+    return m_responsesRouted;
+}
+
+uint64_t
+LoadBalancer::GetClientRx() const
+{
+    return m_clientRx;
+}
+
+uint64_t
+LoadBalancer::GetBackendRx() const
+{
+    return m_backendRx;
+}
+
+void
+LoadBalancer::StartApplication()
+{
+    NS_LOG_FUNCTION(this);
+
+    // Initialize scheduler with cluster
+    if (m_scheduler)
+    {
+        m_scheduler->Initialize(m_cluster);
+    }
+    else
+    {
+        NS_LOG_WARN("No scheduler configured, creating default RoundRobinScheduler");
+        m_scheduler = CreateObject<RoundRobinScheduler>();
+        m_scheduler->Initialize(m_cluster);
+    }
+
+    // Connect to all backends first
+    ConnectToBackends();
+
+    // Create listening socket for clients
+    if (!m_listenSocket)
+    {
+        m_listenSocket = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
+        InetSocketAddress local = InetSocketAddress(Ipv4Address::GetAny(), m_port);
+
+        if (m_listenSocket->Bind(local) == -1)
+        {
+            NS_FATAL_ERROR("Failed to bind load balancer socket to port " << m_port);
+        }
+
+        m_listenSocket->Listen();
+        m_listenSocket->SetAcceptCallback(
+            MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
+            MakeCallback(&LoadBalancer::HandleClientAccept, this));
+        m_listenSocket->SetCloseCallbacks(
+            MakeCallback(&LoadBalancer::HandleClientClose, this),
+            MakeCallback(&LoadBalancer::HandleClientError, this));
+
+        NS_LOG_INFO("LoadBalancer listening on port " << m_port
+                    << " with " << m_cluster.GetN() << " backends");
+    }
+}
+
+void
+LoadBalancer::StopApplication()
+{
+    NS_LOG_FUNCTION(this);
+
+    // Close listening socket
+    if (m_listenSocket)
+    {
+        m_listenSocket->Close();
+        m_listenSocket->SetAcceptCallback(
+            MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
+            MakeNullCallback<void, Ptr<Socket>, const Address&>());
+    }
+
+    // Close all client sockets
+    for (auto& socket : m_clientSockets)
+    {
+        socket->Close();
+    }
+    m_clientSockets.clear();
+
+    // Close all backend sockets
+    for (auto& socket : m_backendSockets)
+    {
+        if (socket)
+        {
+            socket->Close();
+        }
+    }
+    m_backendSockets.clear();
+}
+
+// ========== Client-facing methods ==========
+
+void
+LoadBalancer::HandleClientAccept(Ptr<Socket> socket, const Address& from)
+{
+    NS_LOG_FUNCTION(this << socket << from);
+    NS_LOG_INFO("Accepted client connection from " << InetSocketAddress::ConvertFrom(from).GetIpv4());
+
+    socket->SetRecvCallback(MakeCallback(&LoadBalancer::HandleClientRead, this));
+    socket->SetCloseCallbacks(MakeCallback(&LoadBalancer::HandleClientClose, this),
+                              MakeCallback(&LoadBalancer::HandleClientError, this));
+    m_clientSockets.push_back(socket);
+}
+
+void
+LoadBalancer::HandleClientRead(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+
+    Ptr<Packet> packet;
+    Address from;
+
+    while ((packet = socket->RecvFrom(from)))
+    {
+        if (packet->GetSize() == 0)
+        {
+            break;
+        }
+
+        m_clientRx += packet->GetSize();
+        NS_LOG_DEBUG("Received " << packet->GetSize() << " bytes from client");
+
+        // Add to buffer for this client
+        auto it = m_clientRxBuffers.find(from);
+        if (it == m_clientRxBuffers.end())
+        {
+            m_clientRxBuffers[from] = packet;
+        }
+        else
+        {
+            it->second->AddAtEnd(packet);
+        }
+
+        // Process complete messages
+        ProcessClientBuffer(socket, from);
+    }
+}
+
+void
+LoadBalancer::ProcessClientBuffer(Ptr<Socket> socket, const Address& from)
+{
+    NS_LOG_FUNCTION(this << socket << from);
+
+    auto it = m_clientRxBuffers.find(from);
+    if (it == m_clientRxBuffers.end())
+    {
+        return;
+    }
+
+    Ptr<Packet> buffer = it->second;
+    static const uint32_t headerSize = OffloadHeader().GetSerializedSize();
+
+    while (buffer->GetSize() > 0)
+    {
+        if (buffer->GetSize() < headerSize)
+        {
+            NS_LOG_DEBUG("Buffer has " << buffer->GetSize() << " bytes, need " << headerSize);
+            break;
+        }
+
+        OffloadHeader header;
+        buffer->PeekHeader(header);
+
+        // Calculate total message size
+        uint64_t inputSize = header.GetInputSize();
+        uint64_t payloadSize = (inputSize > headerSize) ? (inputSize - headerSize) : 0;
+        uint64_t totalMessageSize = headerSize + payloadSize;
+
+        if (buffer->GetSize() < totalMessageSize)
+        {
+            NS_LOG_DEBUG("Buffer has " << buffer->GetSize()
+                         << " bytes, need " << totalMessageSize);
+            break;
+        }
+
+        // Remove header
+        buffer->RemoveHeader(header);
+
+        // Extract payload
+        Ptr<Packet> payload = Create<Packet>();
+        if (payloadSize > 0)
+        {
+            payload = buffer->CreateFragment(0, payloadSize);
+            buffer->RemoveAtStart(payloadSize);
+        }
+
+        // Forward the task
+        if (header.GetMessageType() == OffloadHeader::TASK_REQUEST)
+        {
+            ForwardTask(header, payload, socket, from);
+        }
+        else
+        {
+            NS_LOG_WARN("Received non-request message from client, ignoring");
+        }
+    }
+
+    if (buffer->GetSize() == 0)
+    {
+        m_clientRxBuffers.erase(it);
+    }
+}
+
+void
+LoadBalancer::HandleClientClose(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+    NS_LOG_INFO("Client disconnected");
+    CleanupClientSocket(socket);
+}
+
+void
+LoadBalancer::HandleClientError(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+    NS_LOG_ERROR("Client socket error");
+    CleanupClientSocket(socket);
+}
+
+void
+LoadBalancer::CleanupClientSocket(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+    m_clientSockets.remove(socket);
+
+    // Remove pending responses for this client
+    for (auto it = m_pendingResponses.begin(); it != m_pendingResponses.end();)
+    {
+        if (it->second.clientSocket == socket)
+        {
+            NS_LOG_DEBUG("Removing pending task " << it->first << " for closed client");
+            it = m_pendingResponses.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+}
+
+// ========== Backend-facing methods ==========
+
+void
+LoadBalancer::ConnectToBackends()
+{
+    NS_LOG_FUNCTION(this);
+
+    m_backendSockets.resize(m_cluster.GetN());
+    m_backendConnected.resize(m_cluster.GetN(), false);
+    m_backendRxBuffers.resize(m_cluster.GetN());
+
+    for (uint32_t i = 0; i < m_cluster.GetN(); i++)
+    {
+        const Cluster::Backend& backend = m_cluster.Get(i);
+
+        Ptr<Socket> socket = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
+
+        // Bind locally
+        if (socket->Bind() == -1)
+        {
+            NS_LOG_ERROR("Failed to bind backend socket " << i);
+            continue;
+        }
+
+        socket->SetConnectCallback(
+            MakeCallback(&LoadBalancer::HandleBackendConnected, this),
+            MakeCallback(&LoadBalancer::HandleBackendFailed, this));
+
+        socket->Connect(backend.address);
+
+        m_backendSockets[i] = socket;
+        m_backendRxBuffers[i] = Create<Packet>();
+        m_socketToBackend[socket] = i;
+
+        NS_LOG_DEBUG("Connecting to backend " << i);
+    }
+}
+
+void
+LoadBalancer::HandleBackendConnected(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+
+    auto it = m_socketToBackend.find(socket);
+    if (it == m_socketToBackend.end())
+    {
+        NS_LOG_ERROR("Unknown socket in HandleBackendConnected");
+        return;
+    }
+    uint32_t backendIndex = it->second;
+
+    NS_LOG_INFO("Connected to backend " << backendIndex);
+    m_backendConnected[backendIndex] = true;
+
+    // Set up receive callback
+    socket->SetRecvCallback(MakeCallback(&LoadBalancer::HandleBackendRead, this));
+}
+
+void
+LoadBalancer::HandleBackendFailed(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+
+    auto it = m_socketToBackend.find(socket);
+    if (it == m_socketToBackend.end())
+    {
+        NS_LOG_ERROR("Unknown socket in HandleBackendFailed");
+        return;
+    }
+    uint32_t backendIndex = it->second;
+
+    NS_LOG_ERROR("Failed to connect to backend " << backendIndex);
+    m_backendConnected[backendIndex] = false;
+}
+
+void
+LoadBalancer::HandleBackendRead(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+
+    auto it = m_socketToBackend.find(socket);
+    if (it == m_socketToBackend.end())
+    {
+        NS_LOG_ERROR("Unknown socket in HandleBackendRead");
+        return;
+    }
+    uint32_t backendIndex = it->second;
+
+    Ptr<Packet> packet;
+    Address from;
+
+    while ((packet = socket->RecvFrom(from)))
+    {
+        if (packet->GetSize() == 0)
+        {
+            break;
+        }
+
+        m_backendRx += packet->GetSize();
+        NS_LOG_DEBUG("Received " << packet->GetSize() << " bytes from backend " << backendIndex);
+
+        // Add to buffer for this backend
+        m_backendRxBuffers[backendIndex]->AddAtEnd(packet);
+
+        // Process complete messages
+        ProcessBackendBuffer(backendIndex);
+    }
+}
+
+void
+LoadBalancer::ProcessBackendBuffer(uint32_t backendIndex)
+{
+    NS_LOG_FUNCTION(this << backendIndex);
+
+    Ptr<Packet> buffer = m_backendRxBuffers[backendIndex];
+    static const uint32_t headerSize = OffloadHeader().GetSerializedSize();
+
+    while (buffer->GetSize() > 0)
+    {
+        if (buffer->GetSize() < headerSize)
+        {
+            break;
+        }
+
+        OffloadHeader header;
+        buffer->PeekHeader(header);
+
+        // For responses, server sends header + outputSize bytes
+        uint64_t outputSize = header.GetOutputSize();
+        uint64_t totalMessageSize = headerSize + outputSize;
+
+        if (buffer->GetSize() < totalMessageSize)
+        {
+            NS_LOG_DEBUG("Backend buffer has " << buffer->GetSize()
+                         << " bytes, need " << totalMessageSize);
+            break;
+        }
+
+        // Remove header
+        buffer->RemoveHeader(header);
+
+        // Extract payload
+        Ptr<Packet> payload = Create<Packet>();
+        if (outputSize > 0)
+        {
+            payload = buffer->CreateFragment(0, outputSize);
+            buffer->RemoveAtStart(outputSize);
+        }
+
+        // Route the response
+        if (header.GetMessageType() == OffloadHeader::TASK_RESPONSE)
+        {
+            RouteResponse(header, payload, backendIndex);
+        }
+        else
+        {
+            NS_LOG_WARN("Received non-response message from backend " << backendIndex);
+        }
+    }
+}
+
+// ========== Task forwarding and response routing ==========
+
+void
+LoadBalancer::ForwardTask(const OffloadHeader& header,
+                          Ptr<Packet> payload,
+                          Ptr<Socket> clientSocket,
+                          const Address& clientAddr)
+{
+    NS_LOG_FUNCTION(this << header.GetTaskId() << clientSocket);
+
+    // Select a backend
+    int32_t backendIndex = m_scheduler->SelectBackend(header, m_cluster);
+
+    if (backendIndex < 0)
+    {
+        NS_LOG_ERROR("No backend available for task " << header.GetTaskId());
+        return;
+    }
+
+    if (!m_backendConnected[backendIndex])
+    {
+        NS_LOG_ERROR("Backend " << backendIndex << " not connected, dropping task "
+                     << header.GetTaskId());
+        return;
+    }
+
+    // Store pending response info
+    PendingResponse pending;
+    pending.clientSocket = clientSocket;
+    pending.clientAddr = clientAddr;
+    pending.arrivalTime = Simulator::Now();
+    pending.backendIndex = static_cast<uint32_t>(backendIndex);
+    m_pendingResponses[header.GetTaskId()] = pending;
+
+    // Reconstruct and send to backend
+    Ptr<Packet> forwardPacket = payload->Copy();
+    forwardPacket->AddHeader(header);
+
+    int sent = m_backendSockets[backendIndex]->Send(forwardPacket);
+    if (sent > 0)
+    {
+        m_tasksForwarded++;
+        m_scheduler->NotifyTaskSent(backendIndex, header);
+        m_taskForwardedTrace(header, backendIndex);
+
+        NS_LOG_INFO("Forwarded task " << header.GetTaskId() << " to backend " << backendIndex);
+    }
+    else
+    {
+        NS_LOG_ERROR("Failed to forward task " << header.GetTaskId()
+                     << " to backend " << backendIndex);
+        m_pendingResponses.erase(header.GetTaskId());
+    }
+}
+
+void
+LoadBalancer::RouteResponse(const OffloadHeader& header, Ptr<Packet> payload, uint32_t backendIndex)
+{
+    NS_LOG_FUNCTION(this << header.GetTaskId() << backendIndex);
+
+    auto it = m_pendingResponses.find(header.GetTaskId());
+    if (it == m_pendingResponses.end())
+    {
+        NS_LOG_WARN("Received response for unknown task " << header.GetTaskId());
+        return;
+    }
+
+    PendingResponse& pending = it->second;
+    Ptr<Socket> clientSocket = pending.clientSocket;
+    Time latency = Simulator::Now() - pending.arrivalTime;
+
+    // Notify scheduler
+    m_scheduler->NotifyTaskCompleted(pending.backendIndex, header.GetTaskId(), latency);
+
+    // Remove from pending
+    m_pendingResponses.erase(it);
+
+    // Check if client socket is still valid
+    if (!clientSocket)
+    {
+        NS_LOG_WARN("Client socket closed before response for task " << header.GetTaskId());
+        return;
+    }
+
+    // Reconstruct and send to client
+    Ptr<Packet> responsePacket = payload->Copy();
+    responsePacket->AddHeader(header);
+
+    int sent = clientSocket->Send(responsePacket);
+    if (sent > 0)
+    {
+        m_responsesRouted++;
+        m_responseRoutedTrace(header, latency);
+
+        NS_LOG_INFO("Routed response for task " << header.GetTaskId()
+                    << " to client (latency=" << latency.GetMilliSeconds() << "ms)");
+    }
+    else
+    {
+        NS_LOG_ERROR("Failed to route response for task " << header.GetTaskId());
+    }
+}
+
+} // namespace ns3
---
+++ model/load-balancer.h
@@ -0,0 +1,252 @@
+/*
+ * Copyright (c) 2025 UCC
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: John Mullan <122331816@umail.ucc.ie>
+ */
+
+#ifndef LOAD_BALANCER_H
+#define LOAD_BALANCER_H
+
+#include "cluster.h"
+#include "node-scheduler.h"
+#include "offload-header.h"
+
+#include "ns3/application.h"
+#include "ns3/inet-socket-address.h"
+#include "ns3/packet.h"
+#include "ns3/ptr.h"
+#include "ns3/socket.h"
+#include "ns3/traced-callback.h"
+
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <vector>
+
+namespace ns3
+{
+
+/**
+ * @ingroup distributed
+ * @brief Load balancer application for distributing tasks across a cluster.
+ *
+ * LoadBalancer sits between OffloadClients and a cluster of OffloadServers.
+ * It receives task requests from clients, selects a backend server using the
+ * configured NodeScheduler, forwards the task, and routes responses back to
+ * the originating client.
+ *
+ * Architecture:
+ * @code
+ * OffloadClient(s) ---> LoadBalancer ---> OffloadServer(s)
+ *                           |                    |
+ *                           | NodeScheduler      | GpuAccelerator
+ *                           | selects backend    | processes task
+ *                           |                    |
+ *                      <--- routes response <----
+ * @endcode
+ *
+ * The LoadBalancer maintains:
+ * - A listening socket for incoming client connections
+ * - Persistent TCP connections to all backend servers
+ * - A mapping from task IDs to client sockets for response routing
+ */
+class LoadBalancer : public Application
+{
+  public:
+    /**
+     * @brief Get the type ID.
+     * @return The object TypeId.
+     */
+    static TypeId GetTypeId();
+
+    /**
+     * @brief Default constructor.
+     */
+    LoadBalancer();
+
+    /**
+     * @brief Destructor.
+     */
+    ~LoadBalancer() override;
+
+    /**
+     * @brief Set the cluster of backend servers.
+     *
+     * Must be called before the application starts.
+     *
+     * @param cluster The cluster to load balance across.
+     */
+    void SetCluster(const Cluster& cluster);
+
+    /**
+     * @brief Get the number of tasks forwarded to backends.
+     * @return Number of tasks forwarded.
+     */
+    uint64_t GetTasksForwarded() const;
+
+    /**
+     * @brief Get the number of responses routed back to clients.
+     * @return Number of responses routed.
+     */
+    uint64_t GetResponsesRouted() const;
+
+    /**
+     * @brief Get the total bytes received from clients.
+     * @return Total bytes received from clients.
+     */
+    uint64_t GetClientRx() const;
+
+    /**
+     * @brief Get the total bytes received from backends.
+     * @return Total bytes received from backends.
+     */
+    uint64_t GetBackendRx() const;
+
+    /**
+     * @brief Trace callback signature for task forwarded events.
+     * @param header The task header.
+     * @param backendIndex The backend index selected.
+     */
+    typedef void (*TaskForwardedCallback)(const OffloadHeader& header, uint32_t backendIndex);
+
+    /**
+     * @brief Trace callback signature for response routed events.
+     * @param header The response header.
+     * @param totalLatency Time from task receipt to response forwarded.
+     */
+    typedef void (*ResponseRoutedCallback)(const OffloadHeader& header, Time totalLatency);
+
+  protected:
+    void DoDispose() override;
+
+  private:
+    void StartApplication() override;
+    void StopApplication() override;
+
+    // ========== Client-facing (incoming) ==========
+
+    /**
+     * @brief Handle a new client connection.
+     */
+    void HandleClientAccept(Ptr<Socket> socket, const Address& from);
+
+    /**
+     * @brief Handle data received from a client.
+     */
+    void HandleClientRead(Ptr<Socket> socket);
+
+    /**
+     * @brief Process buffered data from a client.
+     */
+    void ProcessClientBuffer(Ptr<Socket> socket, const Address& from);
+
+    /**
+     * @brief Handle client socket close.
+     */
+    void HandleClientClose(Ptr<Socket> socket);
+
+    /**
+     * @brief Handle client socket error.
+     */
+    void HandleClientError(Ptr<Socket> socket);
+
+    /**
+     * @brief Clean up state for a closed client socket.
+     */
+    void CleanupClientSocket(Ptr<Socket> socket);
+
+    // ========== Backend-facing (outgoing) ==========
+
+    /**
+     * @brief Connect to all backend servers.
+     */
+    void ConnectToBackends();
+
+    /**
+     * @brief Handle successful backend connection.
+     */
+    void HandleBackendConnected(Ptr<Socket> socket);
+
+    /**
+     * @brief Handle failed backend connection.
+     */
+    void HandleBackendFailed(Ptr<Socket> socket);
+
+    /**
+     * @brief Handle data received from a backend.
+     */
+    void HandleBackendRead(Ptr<Socket> socket);
+
+    /**
+     * @brief Process buffered data from a backend.
+     */
+    void ProcessBackendBuffer(uint32_t backendIndex);
+
+    // ========== Task forwarding and response routing ==========
+
+    /**
+     * @brief Forward a task to a backend.
+     */
+    void ForwardTask(const OffloadHeader& header,
+                     Ptr<Packet> payload,
+                     Ptr<Socket> clientSocket,
+                     const Address& clientAddr);
+
+    /**
+     * @brief Route a response back to the client.
+     */
+    void RouteResponse(const OffloadHeader& header, Ptr<Packet> payload, uint32_t backendIndex);
+
+    // ========== Configuration ==========
+    uint16_t m_port;               //!< Port to listen on
+    Ptr<NodeScheduler> m_scheduler; //!< The scheduling policy
+
+    // ========== Runtime state ==========
+    Cluster m_cluster;                        //!< Backend servers
+    Ptr<Socket> m_listenSocket;               //!< Listening socket for clients
+
+    // Client connections
+    std::list<Ptr<Socket>> m_clientSockets;   //!< Connected client sockets
+
+    /**
+     * @brief Hash function for Address (for use in unordered_map).
+     */
+    struct AddressHash
+    {
+        std::size_t operator()(const Address& addr) const;
+    };
+
+    std::unordered_map<Address, Ptr<Packet>, AddressHash> m_clientRxBuffers; //!< Per-client receive buffers
+
+    // Backend connections
+    std::vector<Ptr<Socket>> m_backendSockets;      //!< Sockets to backends
+    std::vector<bool> m_backendConnected;           //!< Connection status per backend
+    std::vector<Ptr<Packet>> m_backendRxBuffers;    //!< Per-backend receive buffers
+    std::map<Ptr<Socket>, uint32_t> m_socketToBackend; //!< Map socket to backend index
+
+    // Response routing: taskId -> pending info
+    struct PendingResponse
+    {
+        Ptr<Socket> clientSocket;  //!< The client socket to send response to
+        Address clientAddr;        //!< The client address
+        Time arrivalTime;          //!< When task was received from client
+        uint32_t backendIndex;     //!< Which backend is processing this task
+    };
+    std::unordered_map<uint64_t, PendingResponse> m_pendingResponses;
+
+    // ========== Statistics ==========
+    uint64_t m_tasksForwarded;    //!< Number of tasks forwarded
+    uint64_t m_responsesRouted;   //!< Number of responses routed
+    uint64_t m_clientRx;          //!< Bytes received from clients
+    uint64_t m_backendRx;         //!< Bytes received from backends
+
+    // ========== Trace sources ==========
+    TracedCallback<const OffloadHeader&, uint32_t> m_taskForwardedTrace;
+    TracedCallback<const OffloadHeader&, Time> m_responseRoutedTrace;
+};
+
+} // namespace ns3
+
+#endif // LOAD_BALANCER_H
