3ec17c36815efea75ae59e6b453efe8e163cd56e
johnmullan <johnmullan1408@gmail.com>
2026-01-15 22:29:54 +0000

    distributed: Updated offload server to use the connection manager

--- model/offload-server.cc
+++ model/offload-server.cc
@@ -8,14 +8,12 @@
 
 #include "offload-server.h"
 
-#include "ns3/address-utils.h"
+#include "tcp-connection-manager.h"
+
 #include "ns3/log.h"
 #include "ns3/node.h"
-#include "ns3/packet.h"
+#include "ns3/pointer.h"
 #include "ns3/simulator.h"
-#include "ns3/socket-factory.h"
-#include "ns3/socket.h"
-#include "ns3/tcp-socket-factory.h"
 #include "ns3/uinteger.h"
 
 namespace ns3
@@ -33,16 +31,16 @@ OffloadServer::GetTypeId()
             .SetParent<Application>()
             .SetGroupName("Distributed")
             .AddConstructor<OffloadServer>()
-            .AddAttribute("Local",
-                          "The local address to bind to",
-                          AddressValue(),
-                          MakeAddressAccessor(&OffloadServer::m_local),
-                          MakeAddressChecker())
             .AddAttribute("Port",
                           "Port on which to listen for incoming connections",
                           UintegerValue(9000),
                           MakeUintegerAccessor(&OffloadServer::m_port),
                           MakeUintegerChecker<uint16_t>())
+            .AddAttribute("ConnectionManager",
+                          "Connection manager for transport (defaults to TCP)",
+                          PointerValue(),
+                          MakePointerAccessor(&OffloadServer::m_connMgr),
+                          MakePointerChecker<ConnectionManager>())
             .AddTraceSource("TaskReceived",
                             "A task request has been received",
                             MakeTraceSourceAccessor(&OffloadServer::m_taskReceivedTrace),
@@ -56,8 +54,7 @@ OffloadServer::GetTypeId()
 
 OffloadServer::OffloadServer()
     : m_port(9000),
-      m_socket(nullptr),
-      m_socket6(nullptr),
+      m_connMgr(nullptr),
       m_accelerator(nullptr),
       m_tasksReceived(0),
       m_tasksCompleted(0),
@@ -76,38 +73,17 @@ OffloadServer::DoDispose()
 {
     NS_LOG_FUNCTION(this);
 
-    // Close all accepted sockets with callback cleanup
-    for (auto& socket : m_socketList)
+    // Clean up ConnectionManager
+    if (m_connMgr)
     {
-        socket->SetRecvCallback(MakeNullCallback<void, Ptr<Socket>>());
-        socket->SetCloseCallbacks(MakeNullCallback<void, Ptr<Socket>>(),
-                                  MakeNullCallback<void, Ptr<Socket>>());
-        socket->Close();
+        m_connMgr->Close();
+        m_connMgr = nullptr;
     }
-    m_socketList.clear();
 
-    // Close listening sockets with callback cleanup
-    if (m_socket)
-    {
-        m_socket->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
-                                    MakeNullCallback<void, Ptr<Socket>, const Address&>());
-        m_socket->SetCloseCallbacks(MakeNullCallback<void, Ptr<Socket>>(),
-                                    MakeNullCallback<void, Ptr<Socket>>());
-        m_socket->Close();
-        m_socket = nullptr;
-    }
-    if (m_socket6)
-    {
-        m_socket6->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
-                                     MakeNullCallback<void, Ptr<Socket>, const Address&>());
-        m_socket6->SetCloseCallbacks(MakeNullCallback<void, Ptr<Socket>>(),
-                                     MakeNullCallback<void, Ptr<Socket>>());
-        m_socket6->Close();
-        m_socket6 = nullptr;
-    }
     m_rxBuffer.clear();
     m_pendingTasks.clear();
     m_accelerator = nullptr;
+
     Application::DoDispose();
 }
 
@@ -135,12 +111,6 @@ OffloadServer::GetPort() const
     return m_port;
 }
 
-Address
-OffloadServer::GetLocalAddress() const
-{
-    return m_local;
-}
-
 void
 OffloadServer::StartApplication()
 {
@@ -160,66 +130,27 @@ OffloadServer::StartApplication()
             MakeCallback(&OffloadServer::OnTaskCompleted, this));
     }
 
-    // Create the listening socket
-    if (!m_socket)
+    // Create default ConnectionManager if not set
+    if (!m_connMgr)
     {
-        m_socket = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
-
-        Address local = m_local;
-        if (local.IsInvalid())
-        {
-            local = InetSocketAddress(Ipv4Address::GetAny(), m_port);
-            NS_LOG_INFO("Binding to port " << m_port);
-        }
-        else if (InetSocketAddress::IsMatchingType(local))
-        {
-            m_port = InetSocketAddress::ConvertFrom(local).GetPort();
-            NS_LOG_INFO("Binding to " << InetSocketAddress::ConvertFrom(local).GetIpv4() << ":"
-                                      << m_port);
-        }
-        else if (Inet6SocketAddress::IsMatchingType(local))
-        {
-            m_port = Inet6SocketAddress::ConvertFrom(local).GetPort();
-            NS_LOG_INFO("Binding to " << Inet6SocketAddress::ConvertFrom(local).GetIpv6() << ":"
-                                      << m_port);
-        }
-
-        if (m_socket->Bind(local) == -1)
-        {
-            NS_FATAL_ERROR("Failed to bind socket");
-        }
-
-        m_socket->Listen();
-        // NOTE: Unlike PacketSink, we do NOT call ShutdownSend() here because
-        // OffloadServer needs to send TASK_RESPONSE messages back to clients
-        // when tasks complete on the GPU.
-        m_socket->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
-                                    MakeCallback(&OffloadServer::HandleAccept, this));
-        m_socket->SetCloseCallbacks(MakeCallback(&OffloadServer::HandlePeerClose, this),
-                                    MakeCallback(&OffloadServer::HandlePeerError, this));
+        m_connMgr = CreateObject<TcpConnectionManager>();
     }
 
-    // Create IPv6 socket if only port was specified (for dual-stack support)
-    if (!m_socket6 && m_local.IsInvalid())
-    {
-        m_socket6 = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
-        Address local6 = Inet6SocketAddress(Ipv6Address::GetAny(), m_port);
+    // Configure ConnectionManager
+    m_connMgr->SetNode(GetNode());
+    m_connMgr->SetReceiveCallback(MakeCallback(&OffloadServer::HandleReceive, this));
 
-        if (m_socket6->Bind(local6) == -1)
-        {
-            NS_LOG_WARN("Failed to bind IPv6 socket");
-            m_socket6 = nullptr;
-        }
-        else
-        {
-            m_socket6->Listen();
-            // NOTE: No ShutdownSend() - we need to send responses on IPv6 too
-            m_socket6->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
-                                         MakeCallback(&OffloadServer::HandleAccept, this));
-            m_socket6->SetCloseCallbacks(MakeCallback(&OffloadServer::HandlePeerClose, this),
-                                         MakeCallback(&OffloadServer::HandlePeerError, this));
-        }
+    // Set close callback for TCP to handle client disconnections
+    Ptr<TcpConnectionManager> tcpConnMgr = DynamicCast<TcpConnectionManager>(m_connMgr);
+    if (tcpConnMgr)
+    {
+        tcpConnMgr->SetCloseCallback(MakeCallback(&OffloadServer::HandleClientClose, this));
     }
+
+    // Bind to port
+    m_connMgr->Bind(m_port);
+
+    NS_LOG_INFO("OffloadServer listening on port " << m_port);
 }
 
 void
@@ -235,105 +166,69 @@ OffloadServer::StopApplication()
             MakeCallback(&OffloadServer::OnTaskCompleted, this));
     }
 
-    // Close listening sockets - clear callbacks before close
-    if (m_socket)
+    // Close ConnectionManager
+    if (m_connMgr)
     {
-        m_socket->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
-                                    MakeNullCallback<void, Ptr<Socket>, const Address&>());
-        m_socket->SetCloseCallbacks(MakeNullCallback<void, Ptr<Socket>>(),
-                                    MakeNullCallback<void, Ptr<Socket>>());
-        m_socket->Close();
-        m_socket = nullptr;
-    }
-    if (m_socket6)
-    {
-        m_socket6->SetAcceptCallback(MakeNullCallback<bool, Ptr<Socket>, const Address&>(),
-                                     MakeNullCallback<void, Ptr<Socket>, const Address&>());
-        m_socket6->SetCloseCallbacks(MakeNullCallback<void, Ptr<Socket>>(),
-                                     MakeNullCallback<void, Ptr<Socket>>());
-        m_socket6->Close();
-        m_socket6 = nullptr;
+        m_connMgr->Close();
     }
 
-    // Close all accepted sockets - clear callbacks before close
-    for (auto& socket : m_socketList)
-    {
-        socket->SetRecvCallback(MakeNullCallback<void, Ptr<Socket>>());
-        socket->SetCloseCallbacks(MakeNullCallback<void, Ptr<Socket>>(),
-                                  MakeNullCallback<void, Ptr<Socket>>());
-        socket->Close();
-    }
-    m_socketList.clear();
+    m_rxBuffer.clear();
 }
 
 void
-OffloadServer::HandleAccept(Ptr<Socket> socket, const Address& from)
+OffloadServer::HandleReceive(Ptr<Packet> packet, const Address& from)
 {
-    NS_LOG_FUNCTION(this << socket << from);
-    NS_LOG_INFO("Accepted connection from " << from);
+    NS_LOG_FUNCTION(this << packet << from);
 
-    socket->SetRecvCallback(MakeCallback(&OffloadServer::HandleRead, this));
-    socket->SetCloseCallbacks(MakeCallback(&OffloadServer::HandlePeerClose, this),
-                              MakeCallback(&OffloadServer::HandlePeerError, this));
-    m_socketList.push_back(socket);
-}
-
-void
-OffloadServer::HandleRead(Ptr<Socket> socket)
-{
-    NS_LOG_FUNCTION(this << socket);
-
-    Ptr<Packet> packet;
-    Address from;
-
-    while ((packet = socket->RecvFrom(from)))
+    if (packet->GetSize() == 0)
     {
-        if (packet->GetSize() == 0)
-        {
-            break;
-        }
-
-        m_totalRx += packet->GetSize();
-        NS_LOG_DEBUG("Received " << packet->GetSize() << " bytes from " << from);
-
-        // Add to buffer for this client (TCP stream reassembly)
-        auto it = m_rxBuffer.find(socket);
-        if (it == m_rxBuffer.end())
-        {
-            m_rxBuffer[socket] = packet;
-        }
-        else
-        {
-            it->second->AddAtEnd(packet);
-        }
-
-        // Try to process complete messages from the buffer
-        ProcessBuffer(socket);
+        return;
     }
+
+    m_totalRx += packet->GetSize();
+    NS_LOG_DEBUG("Received " << packet->GetSize() << " bytes from " << from);
+
+    // Add to buffer for this client
+    auto it = m_rxBuffer.find(from);
+    if (it == m_rxBuffer.end())
+    {
+        m_rxBuffer[from] = packet->Copy();
+    }
+    else
+    {
+        it->second->AddAtEnd(packet);
+    }
+
+    // Try to process complete messages from the buffer
+    ProcessBuffer(from);
 }
 
 void
-OffloadServer::ProcessBuffer(Ptr<Socket> socket)
+OffloadServer::HandleClientClose(const Address& clientAddr)
 {
-    NS_LOG_FUNCTION(this << socket);
+    NS_LOG_FUNCTION(this << clientAddr);
+    NS_LOG_INFO("Client disconnected: " << clientAddr);
+    CleanupClient(clientAddr);
+}
 
-    auto it = m_rxBuffer.find(socket);
+void
+OffloadServer::ProcessBuffer(const Address& clientAddr)
+{
+    NS_LOG_FUNCTION(this << clientAddr);
+
+    auto it = m_rxBuffer.find(clientAddr);
     if (it == m_rxBuffer.end())
     {
         return;
     }
 
     Ptr<Packet> buffer = it->second;
-
-    // Header size is constant (33 bytes)
     static const uint32_t headerSize = OffloadHeader::SERIALIZED_SIZE;
 
     // Process all complete messages in the buffer
     while (buffer->GetSize() > 0)
     {
         // Check if we have enough for the header
-        OffloadHeader header;
-
         if (buffer->GetSize() < headerSize)
         {
             NS_LOG_DEBUG("Buffer has " << buffer->GetSize() << " bytes, need " << headerSize
@@ -342,6 +237,7 @@ OffloadServer::ProcessBuffer(Ptr<Socket> socket)
         }
 
         // Peek at the header to get input size
+        OffloadHeader header;
         buffer->PeekHeader(header);
 
         // Calculate total message size (header + payload)
@@ -366,7 +262,7 @@ OffloadServer::ProcessBuffer(Ptr<Socket> socket)
         }
 
         // Process the task
-        ProcessTask(header, socket);
+        ProcessTask(header, clientAddr);
     }
 
     // Update or remove the buffer entry
@@ -377,9 +273,9 @@ OffloadServer::ProcessBuffer(Ptr<Socket> socket)
 }
 
 void
-OffloadServer::ProcessTask(const OffloadHeader& header, Ptr<Socket> socket)
+OffloadServer::ProcessTask(const OffloadHeader& header, const Address& clientAddr)
 {
-    NS_LOG_FUNCTION(this << header.GetTaskId() << socket);
+    NS_LOG_FUNCTION(this << header.GetTaskId() << clientAddr);
 
     if (header.GetMessageType() != OffloadHeader::TASK_REQUEST)
     {
@@ -410,7 +306,7 @@ OffloadServer::ProcessTask(const OffloadHeader& header, Ptr<Socket> socket)
 
     // Track the pending task for response routing
     PendingTask pending;
-    pending.socket = socket;
+    pending.clientAddr = clientAddr;
     pending.task = task;
     m_pendingTasks[header.GetTaskId()] = pending;
 
@@ -429,30 +325,24 @@ OffloadServer::OnTaskCompleted(Ptr<const Task> task, Time duration)
     if (it == m_pendingTasks.end())
     {
         // Task not found in our pending map - it may have been submitted by
-        // another source (e.g., TaskGenerator) directly to the accelerator
+        // another source directly to the accelerator
         NS_LOG_DEBUG("Task " << task->GetTaskId() << " not found in pending tasks (not ours)");
         return;
     }
 
-    Ptr<Socket> socket = it->second.socket;
+    Address clientAddr = it->second.clientAddr;
     Ptr<ComputeTask> computeTask = it->second.task;
     m_pendingTasks.erase(it);
 
-    // Check if socket is still valid
-    if (socket)
-    {
-        SendResponse(socket, computeTask, duration);
-    }
-    else
-    {
-        NS_LOG_WARN("Client socket closed before task " << task->GetTaskId() << " completed");
-    }
+    SendResponse(clientAddr, computeTask, duration);
 }
 
 void
-OffloadServer::SendResponse(Ptr<Socket> socket, Ptr<const ComputeTask> task, Time duration)
+OffloadServer::SendResponse(const Address& clientAddr,
+                            Ptr<const ComputeTask> task,
+                            Time duration)
 {
-    NS_LOG_FUNCTION(this << socket << task->GetTaskId() << duration);
+    NS_LOG_FUNCTION(this << clientAddr << task->GetTaskId() << duration);
 
     // Create response header
     OffloadHeader response;
@@ -467,53 +357,28 @@ OffloadServer::SendResponse(Ptr<Socket> socket, Ptr<const ComputeTask> task, Tim
     Ptr<Packet> packet = Create<Packet>(outputSize);
     packet->AddHeader(response);
 
-    // Send response
-    int sent = socket->Send(packet);
-    if (sent > 0)
-    {
-        m_tasksCompleted++;
-        m_taskCompletedTrace(response, duration);
+    // Send response via ConnectionManager
+    m_connMgr->Send(packet, clientAddr);
 
-        NS_LOG_INFO("Sent response for task "
-                    << task->GetTaskId() << " (output=" << outputSize << " bytes"
-                    << ", duration=" << duration.GetMilliSeconds() << "ms)");
-    }
-    else
-    {
-        NS_LOG_ERROR("Failed to send response for task " << task->GetTaskId());
-    }
+    m_tasksCompleted++;
+    m_taskCompletedTrace(response, duration);
+
+    NS_LOG_INFO("Sent response for task " << task->GetTaskId() << " (output=" << outputSize
+                                          << " bytes, duration=" << duration.GetMilliSeconds()
+                                          << "ms)");
 }
 
 void
-OffloadServer::HandlePeerClose(Ptr<Socket> socket)
+OffloadServer::CleanupClient(const Address& clientAddr)
 {
-    NS_LOG_FUNCTION(this << socket);
-    NS_LOG_INFO("Client disconnected from port " << m_port);
-    CleanupSocket(socket);
-}
+    NS_LOG_FUNCTION(this << clientAddr);
 
-void
-OffloadServer::HandlePeerError(Ptr<Socket> socket)
-{
-    NS_LOG_FUNCTION(this << socket);
-    NS_LOG_ERROR("Socket error on connection");
-    CleanupSocket(socket);
-}
-
-void
-OffloadServer::CleanupSocket(Ptr<Socket> socket)
-{
-    NS_LOG_FUNCTION(this << socket);
-
-    // Remove from socket list
-    m_socketList.remove(socket);
-
-    // Clear pending tasks for this socket
+    // Clear pending tasks for this client
     for (auto it = m_pendingTasks.begin(); it != m_pendingTasks.end();)
     {
-        if (it->second.socket == socket)
+        if (it->second.clientAddr == clientAddr)
         {
-            NS_LOG_DEBUG("Removing pending task " << it->first << " for closed socket");
+            NS_LOG_DEBUG("Removing pending task " << it->first << " for disconnected client");
             it = m_pendingTasks.erase(it);
         }
         else
@@ -522,8 +387,8 @@ OffloadServer::CleanupSocket(Ptr<Socket> socket)
         }
     }
 
-    // Clean up receive buffer for this socket
-    auto bufferIt = m_rxBuffer.find(socket);
+    // Clean up receive buffer for this client
+    auto bufferIt = m_rxBuffer.find(clientAddr);
     if (bufferIt != m_rxBuffer.end())
     {
         NS_LOG_DEBUG("Removing rx buffer for disconnected client");
